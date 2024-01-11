16559356bf2ef822bdf3301df73f01232e2595da
sam1234g <sam_german@hotmail.co.uk>
2024-01-11 22:05:43 +0000

    link target  error fixed

--- app/components/header components/Menu_Items.js
+++ app/components/header components/Menu_Items.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
 
 export default function MenuItems({data, handleOnClick}) {
-    return (
+  const target = data.link.substring(0, 4) === "http" ? "_blank" : "_self";
+
+      
+  return (
         <li onClick={() => handleOnClick()}>
-        <Link href={data.link} target="_blank" className="block px-4 py-2">{data.label}</Link>
+        <Link href={data.link} target={target} className="block px-4 py-2">{data.label}</Link>
       </li>
     );
   }
